cea3c5c3c7b469f6232f07d9d94f6a695be85d94
Lisanne Ardern <ardern.lisanne@gmail.com>
2021-05-28 10:57:41 +0200

    Add error message

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ app.use(morgan('dev'))
 app.get('/', function (req, res) {
   res.send('hello')
 })
+app.get('/error', function (req, res) {
+  throw new Error('oops I made a mistake')
+})
 
 app.use(function (req, res) {
   res.status(404).send('Not found')
+})
+
+app.use(function (err, req, res, next) {
+  console.error(err.stack)
+  res.status(500).send(err.message)
 })
 
 app.listen(3000, function () {
